6a8f75a6cea4175a47abbf0233d5d07aca8d0bc9
Lucian <42606+exaluc@users.noreply.github.com>
2023-11-17 10:13:49 +0100

    python version to min ^3.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docsible',
-    version='0.4.10',
+    version='0.4.11',
     packages=find_packages(),
     include_package_data=True,
     author='Lucian BLETAN',
