1c2e5314da493df2eac18b10a7364a66b9e8606f
Lucian <42606+gni@users.noreply.github.com>
2025-06-02 00:16:46 +0200

    bump version to 0.7.23

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='docsible',
-    version='0.7.22',
+    version='0.7.23',
     packages=find_packages(),
     include_package_data=True,
     author='Lucian BLETAN',
