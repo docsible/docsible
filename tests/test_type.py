805b476612f79858f0c3f7fc9ff58ebfa377a901
dbrennand <52419383+dbrennand@users.noreply.github.com>
2025-11-12 21:50:51 +0000

    tests: add tests to verify behavior is preserved and new feature is working as expected

---
+++ tests/test_type.py
@@ -0,0 +1,17 @@
+import pytest
+from docsible.utils.yaml import load_yaml_file_custom
+
+def test_type_line():
+    result = load_yaml_file_custom("fixtures/type_fixture.yml")
+    # Type is overriden in the fixture to str
+    assert result['test']['type'] == "str"
+    # Type is overriden in the fixture to int
+    assert result['test2']['type'] == "int"
+    # This is not overriden in the fixture, so it should be int
+    # Original behavior is to use the type of the value
+    assert result['test3']['type'] == "int"
+    # This is not overriden in the fixture
+    assert result['test4']['type'] == "list"
+    assert result['test4.0']['type'] == "int"
+    assert result['test4.1']['type'] == "bool"
+    assert result['test4.2']['type'] == "str"
